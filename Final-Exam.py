be662d994f65a45574356e9d2fb5bb4223de08f8
Lauren Meade <lauren.meade@gulllakecs.org>
2019-03-07 12:42:57 -0500

    Started making Hangman game with string and a print command

---
+++ Final-Exam.py
@@ -0,0 +1,13 @@
+# Lauren Meade
+# 3.7.19
+# Final Project:
+
+
+"""
+project explanation
+
+"""
+
+string = "hello"
+print("Let's play hangman! The word you have to guess is", len(string), "letters.")
+
