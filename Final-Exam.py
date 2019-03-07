b140923fb22f457e877b98c9d7b8e850fa01f077
Lauren Meade <lauren.meade@gulllakecs.org>
2019-03-07 12:59:09 -0500

    added guess with a for loop and right and wrong detection with an if statement

--- Final-Exam.py
+++ Final-Exam.py
@@ -11,3 +11,11 @@ project explanation
 string = "hello"
 print("Let's play hangman! The word you have to guess is", len(string), "letters.")
 
+
+num_of_guesses = 7
+for i in range(num_of_guesses):
+    guess = input("Guess a letter: ")
+    if guess not in string:
+        print("You got it wrong! You lose a limb.")
+    else:
+        print("You got it right!")
