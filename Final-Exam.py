b141439fbd330419ddb3dc32a0b15de820a29bdc
Lauren <48028326+StarDust03@users.noreply.github.com>
2019-03-08 17:26:07 -0500

    Update Final-Exam.py

--- Final-Exam.py
+++ Final-Exam.py
@@ -4,18 +4,22 @@
 
 
 """
-project explanation
+This program here allows a user to play Hangman. It first greats the user saying "Let's play hangman!". Then it tells the user how many 
+letters long the word is. Then there is a for loop asking the user for some input and an if statement that compares to see if the letter
+user entered is in the word. The goal is to get to after the user inputs a letter to show the blank lines and the letter in those blank 
+lines if the letter is correct. If not it says the user is wrong and does not input the letter into one of the blank lines but still shows
+the user what is left in the blank lines. 
 
 """
 
-string = "hello"
-print("Let's play hangman! The word you have to guess is", len(string), "letters.")
+word = "hello"
+print("Let's play hangman! The word you have to guess is", len(word), "letters.")
 
 
 num_of_guesses = 7
 for i in range(num_of_guesses):
     guess = input("Guess a letter: ")
-    if guess not in string:
+    if guess not in word:
         print("You got it wrong! You lose a limb.")
     else:
         print("You got it right!")
