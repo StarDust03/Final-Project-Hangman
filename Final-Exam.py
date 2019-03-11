df3a949032ad2df6d970bef64ae22d785df8cf75
Lauren Meade <lauren.meade@gulllakecs.org>
2019-03-11 12:21:12 -0400

    Created new branch and testing new code

--- Final-Exam.py
+++ Final-Exam.py
@@ -4,11 +4,11 @@
 
 
 """
-This program here allows a user to play Hangman. It first greats the user saying "Let's play hangman!". Then it tells the user how many 
-letters long the word is. Then there is a for loop asking the user for some input and an if statement that compares to see if the letter
-user entered is in the word. The goal is to get to after the user inputs a letter to show the blank lines and the letter in those blank 
-lines if the letter is correct. If not it says the user is wrong and does not input the letter into one of the blank lines but still shows
-the user what is left in the blank lines. 
+This program here allows a user to play Hangman. It first greats the user saying "Let's play hangman!". Then it tells
+the user how many letters long the word is. Then there is a for loop asking the user for some input and an if statement
+that compares to see if the letter user entered is in the word. The goal is to get to after the user inputs a letter to
+show the blank lines and the letter in those blank lines if the letter is correct. If not it says the user is wrong and
+does not input the letter into one of the blank lines but still shows the user what is left in the blank lines.
 
 """
 
@@ -23,3 +23,6 @@ for i in range(num_of_guesses):
         print("You got it wrong! You lose a limb.")
     else:
         print("You got it right!")
+
+
+
